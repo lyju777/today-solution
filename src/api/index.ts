42a5d0b81a0427d5bc003bddc8d5022224968b21
lyju777 <lyju777@hanmail.net>
2024-11-07 19:10:11 +0900

    baseUrl 변경

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 axios.defaults.withCredentials = true; // withCredentials 전역설정
+// axios.defaults.baseURL = import.meta.env.VITE_BASE_URL; // baseURL 전역설정
 export const requestAxios = axios.create({
-  baseURL: `${import.meta.env.VITE_BASE_URL}/api`,
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
