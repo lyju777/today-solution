a52ed027cd3f92b0150ab236779f6f0485da3d93
lyju777 <lyju777@hanmail.net>
2024-11-07 21:58:41 +0900

    axios baseUrl env설정

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 
 axios.defaults.withCredentials = true; // withCredentials 전역설정
-axios.defaults.baseURL = `${import.meta.env.VITE_BASE_URL}/api`; // baseURL 전역설정
+if (import.meta.env.MODE === "production") {
+  axios.defaults.baseURL = `${import.meta.env.VITE_BASE_URL}/api`; // prod 환경
+} else {
+  console.log(import.meta.env.VITE_BASE_URL);
+  axios.defaults.baseURL = "/api"; // local 환경
+}
 export const requestAxios = axios.create({
-  // baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
