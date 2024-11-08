9d83a3c2828fbfc26d0daad7e664fe8ed01c6a9d
lyju777 <lyju777@hanmail.net>
2024-11-08 11:26:50 +0900

    server cors설정으로 proxy제거

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,7 @@
 import axios from "axios";
 
 axios.defaults.withCredentials = true; // withCredentials 전역설정
-if (import.meta.env.MODE === "production") {
-  axios.defaults.baseURL = `${import.meta.env.VITE_BASE_URL}/api`; // prod 환경
-} else {
-  console.log(import.meta.env.VITE_BASE_URL);
-  axios.defaults.baseURL = "/api"; // local 환경
-}
+axios.defaults.baseURL = `${import.meta.env.VITE_BASE_URL}/api`; // baseURL 전역설정
 export const requestAxios = axios.create({
   headers: {
     "Content-Type": "application/json",
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default ({ mode }: { mode: string }) => {
-  // 현재 작업 디렉터리의 `mode`를 기반으로 env 파일을 불러옴
-  // 세 번째 매개변수를 ''로 설정하면 `VITE_` 접두사에 관계없이 모든 환경 변수를 불러옴
-  const env = loadEnv(mode, process.cwd(), "");
-
+export default () => {
   return defineConfig({
     plugins: [react()],
     css: {
@@ -19,15 +15,6 @@ export default ({ mode }: { mode: string }) => {
     server: {
       host: "0.0.0.0", // 네트워크 IP 주소
       open: true, // 서버 시작 시 브라우저 열기
-      proxy: {
-        "/api": {
-          target: env.VITE_BASE_URL,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-          secure: false,
-          ws: true,
-        },
-      },
     },
   });
 };
